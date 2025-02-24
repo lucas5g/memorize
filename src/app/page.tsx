a67bed6acc426bde230d33ab5f11a2651d7e8ce3
lucas.sousa <lucas.sousa@codificar.com.br>
2025-02-24 18:25:29 -0300

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default async function Home() {
   const { data } = await phraseService.findMany();
 
   return (
-    <main className="flex gap-2 xl:flex-row flex-col">
+    <main className='flex gap-2 xl:flex-row flex-col'>
       <Card>
-        <h1 className="text-2xl mb-2">Memorize</h1>
+        <h1 className='text-2xl mb-2'>Memorize</h1>
         <Form />
       </Card>
       <Table phrases={data} />
